2977b7abd62de87743498cf26927ae745c978230
Ryland Herrick <ryalnd@gmail.com>
2015-02-02 16:36:08 -0600

    Add some basic User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new }
+
+  it 'has a name' do
+    expect(user).to respond_to(:name)
+  end
+
+  it 'has a birthday' do
+    expect(user).to respond_to(:birthday)
+  end
 end
